32d62ab50f783c407396815f27363f9212a22cf6
pusanja <88085537+pusanja@users.noreply.github.com>
2022-10-07 10:00:03 +0530

    Angular Reactive Forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { CommonModule } from '@angular/common';
+import { from } from 'rxjs';
+const routes: Routes = [
+{path: 'list', component: ListEmployeesComponent },
+{path: 'create', component: CreateEmployeeComponent},
+{path: '',redirectTo:'/list', pathMatch: 'full'}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
