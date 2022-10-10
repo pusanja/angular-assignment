1db99f5e368e917cdf088b4008c8cc6a164af9cc
pusanja <88085537+pusanja@users.noreply.github.com>
2022-10-10 12:39:34 +0530

    Dynamically adding or removing form control validators in reactive form

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -11,6 +11,7 @@ export class CreateEmployeeComponent implements OnInit {
   formErrors = {
     'fullName': '',
     'email': '',
+    'phone':'',
     'skillName': '',
     'experienceInYears': '',
     'proficiency': ''
@@ -24,6 +25,9 @@ export class CreateEmployeeComponent implements OnInit {
     },
     'email': {
       'required': 'Email is required.'
+    },
+    'phone': {
+      'required': 'phone is required.'
     },
     'skillName': {
       'required': 'Skill Name is required.',
@@ -40,17 +44,23 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm=this.fb.group({
       fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
+      contactPreference:['email'],
       email:['',Validators.required],
+      phone:['',],
         skills: this.fb.group({
           skillName:['',Validators.required],
           experienceInYears:['',Validators.required],
           proficiency:['',Validators.required]
         })
      });
+     this.employeeForm.get('contactPreference')?.valueChanges.subscribe((data:string)=>{
+     this.onContactPreferenceChange(data);
+     });
      this.employeeForm.valueChanges.subscribe((data)=>{
       this.logValidationErrors(this.employeeForm);
      });
     }
+
   onSubmit(): void{
     console.log(this.employeeForm.touched);
     console.log(this.employeeForm.value);
@@ -59,6 +69,15 @@ export class CreateEmployeeComponent implements OnInit {
     console.log(this.employeeForm.get('email')?.value);
 
 
+  }
+  onContactPreferenceChange(selectedValue:string){
+    const phoneControl=this.employeeForm.get('phone');
+    if(selectedValue=='phone'){
+      phoneControl?.setValidators(Validators.required);
+    }else{
+      phoneControl?.clearValidators();
+    }
+    phoneControl?.updateValueAndValidity();
   }
   logValidationErrors(group:FormGroup=this.employeeForm): void{
     Object.keys(group.controls).forEach((Key: string)=>{
