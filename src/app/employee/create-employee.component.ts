1c891e5b2b82a8c94773ddb11a7f393d8111978d
pusanja <88085537+pusanja@users.noreply.github.com>
2022-10-11 09:21:51 +0530

    How to make angular custom validator reusable

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators, AbstractControl } from '@angular/forms';
-
+import { CustomValidators } from '../shared/custom.validators';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -46,7 +46,7 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm=this.fb.group({
       fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
       contactPreference:['email'],
-      email:['',[Validators.required,emailDomain('dell.com')]],
+      email:['',[Validators.required, CustomValidators.emailDomain('dell.com')]],
       phone:['',],
         skills: this.fb.group({
           skillName:['',Validators.required],
@@ -109,19 +109,7 @@ export class CreateEmployeeComponent implements OnInit {
 
     }
   }
-     function emailDomain(domainName: string){
-      return (control: AbstractControl): {[key:string]:any}|null=>{
-      const email: string=control.value
-      const domain=email.substring(email.lastIndexOf('@')+1);
-      if( email==="" ||domain.toLowerCase()==='dell.com'){
-        return null;
-      }
-        else{
-          return{'emailDomain':true};
-        }
 
-      };
-    }
 
 
 
---
+++ src/app/shared/custom.validators.ts
@@ -0,0 +1,19 @@
+
+
+import{AbstractControl} from '@angular/forms';
+export class CustomValidators{
+  static emailDomain(domainName: string){
+    return (control: AbstractControl): {[key:string]:any}|null=>{
+    const email: string=control.value
+    const domain=email.substring(email.lastIndexOf('@')+1);
+    if( email==="" ||domain.toLowerCase()==='dell.com'){
+      return null;
+    }
+      else{
+        return{'emailDomain':true};
+      }
+
+    };
+  }
+
+}
